36d9b47c245e1594f89d184e64fdbdfc723f7ff9
amolinat <amolinat@everis.com>
2019-11-11 18:46:18 +0100

    assign policy to MG scope

--- mobules/managetment-groups/optimum-scf-foundation/optimum-scf-foundation.tf
+++ mobules/managetment-groups/optimum-scf-foundation/optimum-scf-foundation.tf
@@ -2,38 +2,24 @@ provider "azurerm" {
     version = "=1.35.0"
 }
 
-# data "azurerm_subscription" "current" {}
-
+# 1. Import Policy test module
 module "azurerm_policy_definition_test" {
-    source = "../../policies/"
+    source = "../../policies/policy-test"
     mg_parent_id = "${var.mg_parent_id}"
 }
 
-resource "azurerm_resource_group" "test" {
-  name     = "test-resources"
-  location = "West Europe"
+# 2. Import json file with assignment parmas info
+data "template_file" "policy_assignment_params" {
+  template = "${file("${path.module}/asgmt-params/asgmt-params-test.json")}"
 }
 
+# 3. Assign policy
 resource "azurerm_policy_assignment" "test" {
     name = "policy-assignment"
-    # scope = "${var.mg_id}"
-    scope = "${azurerm_resource_group.test.id}"
-    # scope = "b1cfce23-b4a8-4d03-bd71-5288e331dbc1"
-    # "${data.azurerm_subscription.current.subscription_id}"
-    # /subscriptions/00000000-0000-0000-000000000000/resourceGroups/myResourceGroup
-    # scope = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/managementGroups/${var.mg_id}"
-   
-    # policy_definition_id = "${azurerm_policy_definition.test.id}"
-    # policy_definition_id = element(module.azurerm_policy_definition_test.policy_id,0)
+    scope = "/providers/Microsoft.Management/managementGroups/${var.mg_id}"
     policy_definition_id    = "${module.azurerm_policy_definition_test.policy_id}"
     description          = "Policy Assignment created via an Acceptance Test"
     display_name         = "My Example Policy Assignment"
-    parameters = <<PARAMETERS
-        {
-        "allowedLocations": {
-            "value": [ "West Europe" ]
-        }
-        }
-        PARAMETERS
+    parameters  = "${data.template_file.policy_assignment_params.rendered}"
 }
 
